061ac1ea7d691b816a4193f70d32eda8b83e766a
Rajat Kumar <rajatkumar199731@gmail.com>
2023-05-30 14:43:52 +0530

    BuyBusy App

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Technology used :-
+# Firebase - For storage of data and authentication
+# React toastify - Allows you to add notifications to your app with ease. It can display the status of network requests and other async actions as a toast.
+# React spinners - A simple React spinner library. It can display a circular loader when fetching data from the database. 
+# react-router-dom - It is the core package containing standard components and functionalities to implement routing in React applications.
+
+
+
---
+++ src/config/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: "buybusy-d1917.appspot.com",
+  messagingSenderId: "946007521541",
+  appId: `${process.env.REACT_APP_APP_ID}`,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+setPersistence(auth, browserLocalPersistence);
+export { db };
